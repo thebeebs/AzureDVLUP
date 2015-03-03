542b19a7f3afb5d518403e894611193a68ce15db
thebeebs <martinbeeby@hotmail.com>
2015-03-03 17:13:46 +0000

    Adding New Site
    Rebuilt the site from the empty asp.net vNext project template. Removed
    Grunt and Bower which were not used but were breaking CDeployment

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace Azure_DVLUP.Controllers
+{
+    public class HomeController : Controller
+    {
+		public IActionResult Index()
+        {
+            return View();
+        }
+		public IActionResult Step1()
+		{
+			return View();
+		}
+		public IActionResult Step2()
+		{
+			return View();
+		}
+		public IActionResult Step3()
+		{
+			return View();
+		}
+	}
+}
